0e9174576dc88da1f2020003e2a57c3348a1d881
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2010-01-17 11:59:36 -0500

    fix indexed fields

--- couchapp/fulltext/all/index.js
+++ couchapp/fulltext/all/index.js
@@ -1,22 +1,33 @@
 function(doc) {
   var output = new Document();
-  var valid_fields = [
-    "mod_datetime",
-    "title",
-    "url",
-    "contents"
-  ];
-  
-  for (var i in valid_fields) {
-    var field = valid_fields[i];
-    output.add(
-      doc[field], 
-      {
-        field: f,
+  var valid_fields = {
+    mod_datetime: {
+        field: "mod_datetime",
         store: "yes",
         index: "analyzed"
-      }
-    )
+    },
+
+    title: {
+      field: "title",
+      store: "yes",
+      index: "analyzed"
+    },
+
+    url: {
+        field: "url",
+        store: "yes",
+        index: "analyzed"
+    },
+    
+    contents:       {
+        field: "contents",
+        store: "no",
+        index: "analyzed"
+    }
+  };
+  
+  for (var field in valid_fields) {
+    output.add(doc[field], valid_fields[field]);
   }
   
   return output;
