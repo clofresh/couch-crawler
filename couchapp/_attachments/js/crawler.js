be20779df0f30ee8e4207f7eba327ed179a7ec14
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2010-01-17 12:05:20 -0500

    search ui

---
+++ couchapp/_attachments/js/crawler.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+  $("#search_form").bind("submit", function() {
+    var url = $("#search_form")[0].action 
+                + "?" + $("#search_form").serialize();
+    $.getJSON(url, 
+      function(data) {
+        var updates = [
+          "search_results_metadata",
+          "search_results",
+          "search_pager"
+        ];
+        
+        _(updates).map(function(section) { 
+          $("#" + section).html(crawler[section](data)) 
+        });
+
+      });
+    return false;
+  });
+});
+
+
+var crawler = {
+  page_to: function(page) {
+    $('#search_form')[0].skip.value = page * $('#search_form')[0].limit.value;
+    $('#search_form').trigger('submit');
+    return false;  
+  },
+  
+  search_results_metadata: function(data) {
+    return $.mustache(
+      "Results {{from}} - {{to}} of {{total_rows}} ({{duration}} seconds)",
+      {
+        from:       data.skip + 1,
+        to:         Math.min(data.skip + data.limit, data.total_rows),
+        total_rows: data.total_rows,
+        duration:   ((data.search_duration 
+                        + data.fetch_duration) / 1000).toFixed(3)
+      }
+    )
+  },
+  
+  search_results: function(data) {
+    return $.mustache(
+      '{{#rows}} <div>{{mod_datetime}} - <a href="{{url}}">{{title}}</a></div> {{/rows}}',
+      {rows: _(data.rows).map( function(row) { return row.fields } )}
+    )
+  },
+  
+  search_pager: function(data) {
+    var num_pages = Math.ceil(data.total_rows / data.limit);
+    var current_page = parseInt(data.skip / data.limit) + 1;
+    
+    return _(_.range(num_pages)).map(function(i) {
+        var page_num = i + 1;
+
+        if (page_num == current_page) {
+          return page_num;
+
+        } else {
+          return $.mustache(
+            '<a href="#" class="pager_link" onclick="return crawler.page_to({{index}})">{{page_num}}</a>', 
+            {"page_num": page_num, "index": i}
+          );
+
+        }
+    }).join(" | ")
+  }
+}
+
+
+
